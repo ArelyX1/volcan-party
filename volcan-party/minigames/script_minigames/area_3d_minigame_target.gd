115e64e1c910d6f01f9aff20c6f0c78ea69d2773
ArelyX1 <andrycaceres2006@gmail.com>
2026-07-21 22:38:47 -0500

    feat: add tunnel

--- volcan-party/minigames/script_minigames/area_3d_minigame_target.gd
+++ volcan-party/minigames/script_minigames/area_3d_minigame_target.gd
@@ -5,5 +5,6 @@ func _on_body_entered(body: Node3D) -> void:
 	var minigame = preload("res://minigames/osugame/osu_minigame.tscn").instantiate()
 	get_tree().current_scene.add_child(minigame)
 	minigame.show()
-	await get_tree().create_timer(10.0).timeout
+	minigame.get_node("Panel/Game").start_game()
+	await get_tree().create_timer(15.0).timeout
 	minigame.queue_free()
